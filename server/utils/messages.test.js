be16a0d3687efe2a698209a5c1b2d4456083a428
Cyril Khan <cyrilkhan8@gmail.com>
2018-02-28 08:22:39 +0100

    added messages.test.js

---
+++ server/utils/messages.test.js
@@ -0,0 +1,12 @@
+var expect = require('expect');
+
+var {generateMessage} = require('./message');
+
+describe('New message generator', () => {
+  it('should generate a correct new message', () => {
+    var text = "You shall not pass";
+    var message = generateMessage(text);
+
+    expect(message.text).toBe(text);
+  });
+});
