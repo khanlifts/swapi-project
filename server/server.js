4c20e2d28274eca12af4e0866336d5cc78cb08bd
Cyril Khan <cyrilkhan8@gmail.com>
2018-03-08 18:17:08 +0100

    added space

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ io.on('connection', (socket) => {
   socket.on('createMessage', (message, callback) => {
     console.log('createMessage: ', message.option, message.number);
 
-    swapi.get(`http://swapi.co/api/${message.option}/${message.number}`).then((result) => {
+    swapi.get(`http://swapi.co/api/${message.option}/${message.number}/`).then((result) => {
     console.log('result: ', result.name);
     socket.emit('newMessage', result.name);
 
