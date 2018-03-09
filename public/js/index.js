8541cecc693750c6c453cfc476552e66d07409b0
Cyril Khan <cyrilkhan8@gmail.com>
2018-03-09 10:07:18 +0100

    added append limit

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,18 @@
 var socket = io();
 
+var maxAppend = 0;
+// on message reception
 socket.on('newMessage', function(message) {
   console.log(message);
 
-  var li = jQuery('<li></li>');
+  var li = jQuery('<div class="col-1-of-3"><li></li></div>');
   li.text(`${message}`);
-  jQuery('#messages').append(li);
+  if (maxAppend % 3 == 0) return;
+  jQuery('#returnInfo').append(li);
+  maxAppend++;
 });
 
+// on form submit
 jQuery('#messages').on('submit', function(e) {
   e.preventDefault();
 
