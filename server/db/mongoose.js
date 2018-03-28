6558eea31d31677b6fd1eac9e44162e1a40f46bb
Cyril Khan <cyrilkhan8@gmail.com>
2018-03-28 05:58:10 +0200

    cleaned up css and server.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,5 @@
 var mongoose = require('mongoose');
 
-// mongoose.Promise = global.Promise;
-// mongoose.connect(process.env.MONGODB_URI);
-
 mongoose.connect('mongodb://localhost/cards');
 
 var db = mongoose.connection;
@@ -10,7 +7,5 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
 
 });
-
-
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -4,14 +4,10 @@ const http = require('http');
 const socketIO = require('socket.io');
 const swapi = require('swapi-node');
 
-// const {ObjectID} = require('mongodb');
-// const {mongoose} = require('./db/mongoose');
-// const {Card} = require('./models/card');
 const {generateMessage} = require('./utils/message');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-
 
 var app = express();
 var server = http.createServer(app);
@@ -20,44 +16,26 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-  console.log('New user connected');
 
   // on createMessage
   socket.on('createMessage', (message, callback) => {
     console.log('createMessage: ', message.option, message.number);
-
+    // get Info from star wars api
     swapi.get(`http://swapi.co/api/${message.option}/${message.number}/`).then((result) => {
     console.log('result: ', result.name);
-
-    // save card
-    // var card = new Card({
-    //   name: result.name
-    // });
-    //
-    // card.save((err, card) => {
-    //   if (err) return console.log(err);
-    //   console.log(`Saved Card: ${card.name}`);;
-    // });
 
     socket.emit('newMessage', result.name);
 
   }).catch((err) => {
     console.log(err);
     socket.emit('newMessage', 'Sorry, no info');
-  });
+    });
 
   });
-
-  // on resetBtn
-  socket.on('resetMessage', () => {
-    // Card.remove({}, (err) => {
-    //   if (err) return handleError(err);
-    // })
-    // console.log('Removed all Cards');
-  })
 });
-
 
 server.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+module.exports = {app}
