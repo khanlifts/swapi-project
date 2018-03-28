7ce812fdfaa2573f98fe6508e70300840d4d93c7
Cyril Khan <cyrilkhan8@gmail.com>
2018-03-28 05:41:16 +0200

    cleaned up index.js file

--- public/js/index.js
+++ public/js/index.js
@@ -13,64 +13,44 @@ window.onload = function() {
 
 // counters & buttonId
 var maxAppend = 0;
-var round = 1;
 var submitButton = jQuery('#yodaBtn');
 var messageNumberbox = jQuery('[name=number]');
+var optionSelect = jQuery('[name=option]')
 // on message reception
 socket.on('newMessage', function(message) {
   console.log(message);
   submitButton.removeAttr('disabled').text('Get Data');
   messageNumberbox.val('');
-
+  // set jQuery elements
+  var liCol = jQuery('<div class="cards"></div>');
+  var li = jQuery('<li></li>');
+  liCol.text(`${message}`);
+  li.text(`${message}`);
   // append in cards
   if (maxAppend >= 12) {
-    round =+ 1;
-    alert(`Here comes round ${round}`);
     jQuery('.cards').remove();
     maxAppend = 0;
     return;
   } else if (maxAppend >= 8) {
-    var liCol = jQuery('<div class="cards"></div>');
-    liCol.text(`${message}`);
     jQuery('#returnInfo2').append(liCol);
-
-    var li = jQuery('<li></li>');
-    li.text(`${message}`);
     jQuery('#card__figure3').append(li);
-
     maxAppend++;
     return;
   } else if (maxAppend >= 4) {
-    var liCol = jQuery('<div class="cards"></div>');
-    liCol.text(`${message}`);
     jQuery('#returnInfo1').append(liCol);
-
-    var li = jQuery('<li></li>');
-    li.text(`${message}`);
     jQuery('#card__figure2').append(li);
-
     maxAppend++;
     return;
   } else {
-    var liCol = jQuery('<div class="cards"></div>');
-    liCol.text(`${message}`);
     jQuery('#returnInfo').append(liCol);
-
-    var li = jQuery('<li></li>');
-    li.text(`${message}`);
     jQuery('#card__figure1').append(li);
-
     maxAppend++;
   }
-
 });
 
 // on form submit
 jQuery('#messages').on('submit', function(e) {
   e.preventDefault();
-
-  var optionSelect = jQuery('[name=option]')
-  var messageNumberbox = jQuery('[name=number]');
 
   submitButton.attr('disabled', 'disabled').text('Loading...');
 
@@ -83,28 +63,19 @@ jQuery('#messages').on('submit', function(e) {
 
 // on form button reset
 jQuery('#resetBtn').on('click', function() {
-  console.log('resetBtn fired on click');
-
   // remove cards in the DOM
   jQuery('.cards').remove();
-  // maxAppend = 0;
-
-  // temporarily commenting out the delete process of db
-  // socket.emit('resetMessage', {});
 });
 
 // on card click to flip
 jQuery('#card1').on('click', function() {
-  console.log('flipped card');
   jQuery('#card1').toggleClass('flipped');
 });
 
 jQuery('#card2').on('click', function() {
-  console.log('flipped card');
   jQuery('#card2').toggleClass('flipped');
 });
 
 jQuery('#card3').on('click', function() {
-  console.log('flipped card');
   jQuery('#card3').toggleClass('flipped');
 });
